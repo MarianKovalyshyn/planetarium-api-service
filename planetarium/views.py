3eee3fca7e95ed6d7c6f12b4081797a06d5484f9
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-10-22 12:23:49 +0300

    Add views for each model

--- planetarium/views.py
+++ planetarium/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from planetarium.models import (
+    AstronomyShow,
+    ShowTheme,
+    ShowSession,
+    PlanetariumDome,
+    Ticket,
+    Reservation,
+)
+from planetarium.serializers import (
+    AstronomyShowSerializer,
+    ShowThemeSerializer,
+    ShowSessionSerializer,
+    PlanetariumDomeSerializer,
+    ReservationSerializer,
+    TicketSerializer,
+)
+
+
+class AstronomyShowViewSet(viewsets.ModelViewSet):
+    queryset = AstronomyShow.objects.prefetch_related("show_theme")
+    serializer_class = AstronomyShowSerializer
+
+
+class ShowThemeViewSet(viewsets.ModelViewSet):
+    queryset = ShowTheme.objects.all()
+    serializer_class = ShowThemeSerializer
+
+
+class ShowSessionViewSet(viewsets.ModelViewSet):
+    queryset = ShowSession.objects.select_related(
+        "astronomy_show", "planetarium_dome"
+    )
+    serializer_class = ShowSessionSerializer
+
+
+class PlanetariumDomeViewSet(viewsets.ModelViewSet):
+    queryset = PlanetariumDome.objects.all()
+    serializer_class = PlanetariumDomeSerializer
+
+
+class TicketViewSet(viewsets.ModelViewSet):
+    queryset = Ticket.objects.select_related("show_session", "reservation")
+    serializer_class = TicketSerializer
+
+
+class ReservationViewSet(viewsets.ModelViewSet):
+    queryset = Reservation.objects.select_related("user")
+    serializer_class = ReservationSerializer
