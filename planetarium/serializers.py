25fd0403ccd5148cf8983a3723053cd023e78828
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-10-22 12:23:08 +0300

    Specify fields in Meta class

--- planetarium/serializers.py
+++ planetarium/serializers.py
@@ -13,28 +13,34 @@ from planetarium.models import (
 class AstronomyShowSerializer(serializers.ModelSerializer):
     class Meta:
         model = AstronomyShow
+        fields = "__all__"
 
 
 class ShowThemeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowTheme
+        fields = "__all__"
 
 
 class ShowSessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShowSession
+        fields = "__all__"
 
 
 class PlanetariumDomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlanetariumDome
+        fields = "__all__"
 
 
 class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
+        fields = "__all__"
 
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
+        fields = "__all__"
