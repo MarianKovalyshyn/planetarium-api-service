f8246c59b9f529db7e96a8aa3636849ccbad3043
Marian Kovalyshyn <mariankovalishin12345@gmail.com>
2023-10-23 18:40:14 +0300

    Replace login by email instead of username

--- planetarium/admin.py
+++ planetarium/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext as _
 
 from .models import (
     AstronomyShow,
@@ -8,6 +9,7 @@ from .models import (
     PlanetariumDome,
     Ticket,
     Reservation,
+    User,
 )
 
 admin.site.register(AstronomyShow)
@@ -16,3 +18,38 @@ admin.site.register(ShowSession)
 admin.site.register(PlanetariumDome)
 admin.site.register(Ticket)
 admin.site.register(Reservation)
+
+
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
+    list_display = ("email", "first_name", "last_name", "is_staff")
+    search_fields = ("email", "first_name", "last_name")
+    ordering = ("email",)
